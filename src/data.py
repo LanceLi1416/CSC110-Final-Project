7cd4b80e492a2221d8ffdc92b1858b0cd47e813e
Jeffery <jefferyzhan84@gmail.com>
2021-12-10 00:56:19 -0500

    new algorithm for getting the scales in range. also, fixed read_csv_file, should work now.

--- src/data.py
+++ src/data.py
@@ -7,18 +7,7 @@ real_data = "../data/COVIDiSTRESS June 17.csv"
 test_data = "../data/sample_data.csv"
 
 
-def generate_interval_list(interval_value: float) -> List[float]:
-    """Return a scale fitted from the values -2 to 2 with intervals of interval_value per element"""
-    start = -2
-    da_row = [start]
-    while start + interval_value < 2.0:
-        start += interval_value
-        da_row.append(round(start, 3))
-    da_row.append(2)
-    return da_row
-
-
-def calc_stress_score(person: List[str]) -> int:
+def calc_stress_score(person: List[str]) -> float:
     """Return stress score from a row of the dataset"""
     # Value added for each response in the survey
     # Depending on the nature of the question we may want to reduce or increase the stress score
@@ -36,143 +25,139 @@ def calc_stress_score(person: List[str]) -> int:
     # List of values from dataset of survey used for calculations
     answer_values = person[21:54] + person[70:109] + person[110:136] + person[137:144]
     # ACCUMULATOR stress_so_far: running sum of stress score
-    stress_so_far = 0
+    stress_so_far = 0.0
     # ACCUMULATOR absolute_index: running index to offset the for loop scaling
     absolute_index = 0
 
     for scale_tuple in category_tuples:
-        # create a fitted interval of -2 to 2 (5 integers between -2 to 2)
-        intervals = 5 / (scale_tuple[0] + 1)
-        da_row = generate_interval_list(intervals)
-        # iterate through each section of questions
-        for i in range(scale_tuple[1]):
-            if answer_values[i + absolute_index] != 'NA' and \
-                    int(answer_values[i + absolute_index]) <= scale_tuple[1]:
-                answer_val = int(answer_values[i + absolute_index])
-                stress_so_far += da_row[answer_val] * stress_method[i + absolute_index]
-        absolute_index += scale_tuple[1]
+        for _ in range(scale_tuple[1]):
+            og_answer = answer_values[absolute_index]
+            if og_answer != 'NA' and int(og_answer) <= scale_tuple[0]:
+                bounded_answer = ((int(og_answer) - 1) * (4 / (scale_tuple[0] - 1))) - 2
+                stress_so_far += bounded_answer * stress_method[absolute_index]
+                # print(f'og: {og_answer}, bounded: {bounded_answer}, scale: {scale_tuple[0]}')
+            absolute_index += 1
     return stress_so_far
 
 
-def process_country() -> Dict[str, tuple[int, int]]:
+def process_country() -> Dict[str, tuple[int, float]]:
     """Return country values for data_dict"""
     countries_dict = {}
     with open('../data/list_of_countries.txt', encoding='utf-8') as file_2:
         for country in file_2:
-            countries_dict[country.strip()] = (0, 0)
-    countries_dict['NA'] = (0, 0)
+            countries_dict[country.strip()] = (0, 0.0)
+    countries_dict['NA'] = (0, 0.0)
     return countries_dict
 
 
-def initialize_data_list() -> List[Dict[str, tuple[int, int]]]:
+def initialize_data_list() -> List[Dict[str, tuple[int, float]]]:
     """Return initialized values"""
     countries = process_country()
     data_start = [
-        # Initialize age
-        {'18-24': (0, 0),
-         '25-34': (0, 0),
-         '35-44': (0, 0),
-         '45-54': (0, 0),
-         '55-64': (0, 0),
-         '65+': (0, 0),
-         'NA': (0, 0)},
-        # Initialize gender
-        {'Male': (0, 0),
-         'Female': (0, 0),
-         'NA': (0, 0)},
-        # Initialize education
-        {'None': (0, 0),
-         'Up to 6 years of school': (0, 0),
-         'Up to 9 years of school': (0, 0),
-         'Up to 12 years of school': (0, 0),
-         'Some College, short continuing education or equivalent': (0, 0),
-         'College degree, bachelor, master': (0, 0),
-         'PhD/Doctorate': (0, 0),
-         'Uninformative response': (0, 0),
-         'NA': (0, 0)},
-        # Initialize employment status
-        {'Not employed': (0, 0),
-         'Student': (0, 0),
-         'Part time employed': (0, 0),
-         'Full time employed': (0, 0),
-         'Self-employed': (0, 0),
-         'Retired': (0, 0),
-         'NA': (0, 0)
+        # Initialize age (column 4)
+        {'18-24': (0, 0.0),
+         '25-34': (0, 0.0),
+         '35-44': (0, 0.0),
+         '45-54': (0, 0.0),
+         '55-64': (0, 0.0),
+         '65+': (0, 0.0)},
+        # Initialize gender (column 5)
+        {'Male': (0, 0.0),
+         'Female': (0, 0.0),
+         'NA': (0, 0.0)},
+        # Initialize education (column 6)
+        {'None': (0, 0.0),
+         'Up to 6 years of school': (0, 0.0),
+         'Up to 9 years of school': (0, 0.0),
+         'Up to 12 years of school': (0, 0.0),
+         'Some College, short continuing education or equivalent': (0, 0.0),
+         'College degree, bachelor, master': (0, 0.0),
+         'PhD/Doctorate': (0, 0.0),
+         'Uninformative response': (0, 0.0),
+         'NA': (0, 0.0)},
+        # Initialize employment status (column 8)
+        {'Not employed': (0, 0.0),
+         'Student': (0, 0.0),
+         'Part time employed': (0, 0.0),
+         'Full time employed': (0, 0.0),
+         'Self-employed': (0, 0.0),
+         'Retired': (0, 0.0),
+         'NA': (0, 0.0)
          },
-        # Initialize country of residence
+        # Initialize country of residence (column 9)
         countries,
-        # Initialize whether they are an expatriate
-        {'yes': (0, 0),
-         'no': (0, 0),
-         'NA': (0, 0)
+        # Initialize whether they are an expatriate (column 10)
+        {'yes': (0, 0.0),
+         'no': (0, 0.0),
+         'NA': (0, 0.0)
          },
-        # Initialize marital status
-        {'Single': (0, 0),
-         'Married/cohabiting': (0, 0),
-         'Divorced/widowed': (0, 0),
-         'Uninformative response': (0, 0),
-         'NA': (0, 0)},
-        # Initialize whether they reside in a high risk group
-        {'Yes': (0, 0),
-         'No': (0, 0),
-         'Not sure': (0, 0),
-         'NA': (0, 0)},
-        # Initialize isolation status
-        {'Life carries on as usual': (0, 0),
-         'Life carries on with minor changes': (0, 0),
-         'Isolated': (0, 0),
-         'Isolated in medical facility of similar location': (0, 0),
-         'NA': (0, 0)},
-        # Initialize adults isolated with participant
-        {'0': (0, 0),
-         '1': (0, 0),
-         '2': (0, 0),
-         '3': (0, 0),
-         '4': (0, 0),
-         '5': (0, 0),
-         '6': (0, 0),
-         '7': (0, 0),
-         '8': (0, 0),
-         '9': (0, 0),
-         '10': (0, 0),
-         '11-20': (0, 0),
-         '21-30': (0, 0),
-         '31-40': (0, 0),
-         '41-50': (0, 0),
-         '51-60': (0, 0),
-         '61-70': (0, 0),
-         '71-80': (0, 0),
-         '81-90': (0, 0),
-         '91-100': (0, 0),
-         '101-110': (0, 0),
-         'NA': (0, 0)},
-        # Initialize children isolated with participant
-        {'0': (0, 0),
-         '1': (0, 0),
-         '2': (0, 0),
-         '3': (0, 0),
-         '4': (0, 0),
-         '5': (0, 0),
-         '6': (0, 0),
-         '7': (0, 0),
-         '8': (0, 0),
-         '9': (0, 0),
-         '10': (0, 0),
-         '11-20': (0, 0),
-         '21-30': (0, 0),
-         '31-40': (0, 0),
-         '41-50': (0, 0),
-         '51-60': (0, 0),
-         '61-70': (0, 0),
-         '71-80': (0, 0),
-         '81-90': (0, 0),
-         '91-100': (0, 0),
-         '101-110': (0, 0),
-         'NA': (0, 0)}]
+        # Initialize marital status (column 12)
+        {'Single': (0, 0.0),
+         'Married/cohabiting': (0, 0.0),
+         'Divorced/widowed': (0, 0.0),
+         'Uninformative response': (0, 0.0),
+         'NA': (0, 0.0)},
+        # Initialize whether they reside in a high risk group (column 14)
+        {'Yes': (0, 0.0),
+         'No': (0, 0.0),
+         'Not sure': (0, 0.0),
+         'NA': (0, 0.0)},
+        # Initialize isolation status (column 15)
+        {'Life carries on as usual': (0, 0.0),
+         'Life carries on with minor changes': (0, 0.0),
+         'Isolated': (0, 0.0),
+         'Isolated in medical facility of similar location': (0, 0.0),
+         'NA': (0, 0.0)},
+        # Initialize adults isolated with participant (column 16)
+        {'0': (0, 0.0),
+         '1': (0, 0.0),
+         '2': (0, 0.0),
+         '3': (0, 0.0),
+         '4': (0, 0.0),
+         '5': (0, 0.0),
+         '6': (0, 0.0),
+         '7': (0, 0.0),
+         '8': (0, 0.0),
+         '9': (0, 0.0),
+         '10': (0, 0.0),
+         '11-20': (0, 0.0),
+         '21-30': (0, 0.0),
+         '31-40': (0, 0.0),
+         '41-50': (0, 0.0),
+         '51-60': (0, 0.0),
+         '61-70': (0, 0.0),
+         '71-80': (0, 0.0),
+         '81-90': (0, 0.0),
+         '91-100': (0, 0.0),
+         '101-110': (0, 0.0),
+         'NA': (0, 0.0)},
+        # Initialize children isolated with participant (column 17)
+        {'0': (0, 0.0),
+         '1': (0, 0.0),
+         '2': (0, 0.0),
+         '3': (0, 0.0),
+         '4': (0, 0.0),
+         '5': (0, 0.0),
+         '6': (0, 0.0),
+         '7': (0, 0.0),
+         '8': (0, 0.0),
+         '9': (0, 0.0),
+         '10': (0, 0.0),
+         '11-20': (0, 0.0),
+         '21-30': (0, 0.0),
+         '31-40': (0, 0.0),
+         '41-50': (0, 0.0),
+         '51-60': (0, 0.0),
+         '61-70': (0, 0.0),
+         '71-80': (0, 0.0),
+         '81-90': (0, 0.0),
+         '91-100': (0, 0.0),
+         '101-110': (0, 0.0),
+         'NA': (0, 0.0)}]
     return data_start
 
 
-def read_csv_file(file_name: str) -> List[Dict[str, tuple[int, int]]]:
+def read_csv_file(file_name: str) -> List[Dict[str, tuple[int, float]]]:
     """Return the data stored in a csv file with the given filename.
 
     The return value is list consisting of 11 dictionaries:
@@ -191,8 +176,8 @@ def read_csv_file(file_name: str) -> List[Dict[str, tuple[int, int]]]:
         is isolating with (edited)
 
     The dictionaries each map to their own tuple of two items,
-    the first being the total stress_score that has been added to them,
-    and the second being the population (or number of people) that have added their score to it
+    the first being the population (or number of people) that have added their score to it
+    and the second being the total stress_score that has been added to them,
     """
     # ACCUMULATOR data_processed_so_far: the running list of
     data_processed_so_far = initialize_data_list()
@@ -209,20 +194,10 @@ def read_csv_file(file_name: str) -> List[Dict[str, tuple[int, int]]]:
     for row in reader:
         stress_score = calc_stress_score(row)
 
-        index = 4
-        category = data_processed_so_far[index - 4]
-        age = int(row[index])
-
-        if row[index] == 'NA':
-            category['NA'] = (category['NA'][0] + 1, category['NA'][1] + stress_score)
-        elif 'other' in row[index].lower():
-            if 'Other/would rather not say' not in category:
-                category['Other/would rather not say'] = (0, 0)
-            category['Other/would rather not say'] = (
-                category['Other/would rather not say'][0] + 1,
-                category['Other/would rather not say'][1] + stress_score
-            )
-        elif 18 <= age <= 24:
+        csv_index = 4
+        category = data_processed_so_far[csv_index - 4]
+        age = int(row[csv_index])
+        if 18 <= age <= 24:
             category['18-24'] = (category['18-24'][0] + 1, category['18-24'][1] + stress_score)
         elif 25 <= age <= 34:
             category['25-34'] = (category['25-34'][0] + 1, category['25-34'][1] + stress_score)
@@ -235,12 +210,12 @@ def read_csv_file(file_name: str) -> List[Dict[str, tuple[int, int]]]:
         elif 65 <= age:
             category['65+'] = (category['65+'][0] + 1, category['65+'][1] + stress_score)
 
-        for index in range(16, 18):
-            category = data_processed_so_far[index - 7]
-            if row[index] == 'NA':
+        for csv_index in range(16, 18):
+            category = data_processed_so_far[csv_index - 7]
+            if row[csv_index] == 'NA':
                 category['NA'] = (category['NA'][0] + 1, category['NA'][1] + stress_score)
                 break
-            num_dependents = int(row[index])
+            num_dependents = int(row[csv_index])
             if num_dependents == 0:
                 category['0'] = (category['0'][0] + 1, category['0'][1] + stress_score)
             elif num_dependents == 1:
@@ -284,19 +259,22 @@ def read_csv_file(file_name: str) -> List[Dict[str, tuple[int, int]]]:
                 category['101-110'] = (
                     category['101-110'][0] + 1, category['101-110'][1] + stress_score)
 
-        for index in {1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}:
-            if row[index] == 'NA':
+        remaining_columns = [5, 6, 8, 9, 10, 12, 14, 15]
+        for csv_index in remaining_columns:
+            category = data_processed_so_far[remaining_columns.index(csv_index) + 1]
+
+            if row[csv_index] == 'NA':
                 category['NA'] = (category['NA'][0] + 1, category['NA'][1] + stress_score)
-            elif 'other' in row[index].lower():
+            elif 'would rather not say' in row[csv_index].lower():
                 if 'Other/would rather not say' not in category:
                     category['Other/would rather not say'] = (0, 0)
                 category['Other/would rather not say'] = (
                     category['Other/would rather not say'][0] + 1,
                     category['Other/would rather not say'][1] + stress_score
                 )
-            elif row[index] not in category:
+            elif row[csv_index] not in category:
                 continue
             else:
-                value = category[row[index]]
-                category[row[index]] = (value[0] + 1, value[1] + stress_score)
+                value = category[row[csv_index]]
+                category[row[csv_index]] = (value[0] + 1, value[1] + stress_score)
     return data_processed_so_far
